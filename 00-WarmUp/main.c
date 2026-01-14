30087fccd506c1faceae41edb818bf452be35616
Rohan Palivela <rohan.palivela@gmail.com>
2026-01-14 00:32:54 -0600

    my soul is empty

--- 00-WarmUp/main.c
+++ 00-WarmUp/main.c
@@ -15,6 +15,7 @@ typedef struct Elevator {
 	long distance_traveled;
 	long total_wait_in_elev;
 	long total_wait_for_elev;
+	long people_off;
 	bool upwards; // true if up, false if down (obviously)
 	bool stopped; 
 	short time_waiting;
@@ -52,8 +53,8 @@ void linked_list_add_end(linked_list* ll, ll_node* node) {
 	ll->tail = node;
 	ll->size++;
 	// printf("%p <- newtail = %d -> %p\n", node->prev, node->p.desired_floor, node->next);
-	ll_node* cur = ll->head;
 	/*
+	ll_node* cur = ll->head;
 	while (cur != NULL) {
 		printf("iterated to find person: %d\n", cur->p.desired_floor);
 		cur = cur->next;
@@ -63,14 +64,18 @@ void linked_list_add_end(linked_list* ll, ll_node* node) {
 
 ll_node* linked_list_remove_first(linked_list* ll) {
 	ll_node* h = ll->head;
-	printf("old head = %d -> %p\n", ll->head->p->desired_floor, ll->head->next);
-	ll->head = ll->head->next;
 	if (ll->size == 1) {
+		ll->head = NULL;
 		ll->tail = NULL;
+		ll->size = 0;
+		printf("new size = 0\n");
+		return h;
 	}
+	// printf("old head = %d -> %p\n", ll->head->p->desired_floor, ll->head->next);
+	ll->head = ll->head->next;
 	ll->size--;
 	if (ll->head != NULL) {
-		printf("new head = %d -> %p\n", ll->head->p->desired_floor, ll->head->next);
+		// printf("new head = %d -> %p\n", ll->head->p->desired_floor, ll->head->next);
 	}
 	printf("new size = %d\n", ll->size);
 	return h;
@@ -101,19 +106,18 @@ void remove_rider_beginning(Elevator* elev, int filled_spots) {
 	elev->riders[0] = NULL;
 
 	for (int i = 1; i < filled_spots; i++) {
-		elev[i - 1] = elev[i]; // shift
+		elev->riders[i - 1] = elev->riders[i]; // shift
 	}
 }
 
 void increment_wait(linked_list* ll) {
 	ll_node* cur = ll->head;
-	printf("cur size: %d\n", ll->size);
 	while (cur != NULL) {
-		// printf("hi\n");
-		printf("cur = %d -> %p\n", cur->p->desired_floor, cur->next);
+		// printf("cur node: %d\n", cur->p->desired_floor);
+		// printf("cur = %d -> %p\n", cur->p->desired_floor, cur->next);
 		// printf("<- cur = %p -> \n", cur->p);
 		cur->p->time_waiting_for_elevator++;
-		printf("new time waiting is: %d\n", cur->p->time_waiting_for_elevator);
+		// printf("new time waiting is: %d\n", cur->p->time_waiting_for_elevator);
 		cur = cur->next;
 	}
 }
@@ -136,7 +140,16 @@ bool keep_going(linked_list* floors[], int floor, bool upwards) {
 	}
 }
 
+void print_array(Person* arr[], int size) {
+	for (int i = 0; i < size; i++) {
+		printf("%d ", arr[i]->desired_floor);
+	}
+	printf("\n");
+}
+
 void handle_stop(Elevator* elevator, linked_list* ll) {
+	print_array(elevator->riders, elevator->people_count);
+	printf("old elevator people_count is %d\n", elevator->people_count);
 	if (elevator->upwards) {
 		int curInd = 0;
 		while (curInd < elevator->people_count && elevator->riders[curInd]->desired_floor == elevator->floor) {
@@ -146,6 +159,7 @@ void handle_stop(Elevator* elevator, linked_list* ll) {
 					, elevator->floor);
 			remove_rider_beginning(elevator, elevator->people_count);
 			elevator->people_count--;
+			elevator->people_off++;
 		}
 	} else {
 		int curInd = elevator->people_count - 1;
@@ -156,16 +170,21 @@ void handle_stop(Elevator* elevator, linked_list* ll) {
 					, elevator->floor);
 			elevator->riders[curInd] = NULL;
 			elevator->people_count--;
+			elevator->people_off++;
+			curInd--;
 		}
 	}
 	while (elevator->people_count < 20 && ll->size > 0) {
 		ll_node* removed = linked_list_remove_first(ll);
 		printf("adding %d rider from linked list to elevator\n", removed->p->desired_floor);
 		printf("added %d to time wait for elevator\n", removed->p->time_waiting_for_elevator);
-		elevator->total_wait_for_elev += removed->p->time_waiting_for_elevator;
 		add_rider_chronological(elevator->riders, removed->p, elevator->upwards, elevator->people_count);
+		elevator->total_wait_for_elev += removed->p->time_waiting_for_elevator;
 		elevator->people_count++;
+		free(removed);
 	}
+
+	printf("new elevator people_count is %d\n", elevator->people_count);
 }
 
 int main() {
@@ -183,6 +202,7 @@ int main() {
 		e->time_waiting = 0;
 		e->total_wait_in_elev = 0;
 		e->total_wait_for_elev = 0;
+		e->people_off = 0;
 		elevators[i] = e;
 	}
 
@@ -205,23 +225,21 @@ int main() {
 	printf("initialized floors\n");
 
 	// simulation start
-	while (time < 13) {
-		printf("ITERATION %d\n", time);
+	while (time < 3600) {
+		printf("\nITERATION %d\n", time);
 		int num_people_in = rand() % 5; // 0-4
 		int num_people_out = rand() % 5; // 0-4
 		printf("%d people coming in, %d people coming out\n", num_people_in, num_people_out);
 
 		for (int i = 0; i < num_people_in; i++) {
 			linked_list* ll = floors[0];
-			// ll_node new_node = {{0, rand() % 150}, NULL, NULL};
 			ll_node* new_node = malloc(sizeof(ll_node));
 			new_node->next = NULL;
 			Person* pers = malloc(sizeof(Person));
 			pers->desired_floor = rand() % 150;
 			pers->time_waiting_for_elevator = 0;
 			new_node->p = pers;
-			printf("adding person wanting to go from floor 0 to floor %d\n", new_node->p->desired_floor);
-
+			// printf("adding person wanting to go from floor 0 to floor %d\n", new_node->p->desired_floor);
 			linked_list_add_end(ll, new_node);
 		}
 
@@ -234,7 +252,7 @@ int main() {
 			pers->desired_floor = 0;
 			pers->time_waiting_for_elevator = 0;
 			new_node->p = pers;
-			printf("adding person wanting to go from floor %d to floor 0\n", cur_passenger_floor);
+			// printf("adding person wanting to go from floor %d to floor 0\n", cur_passenger_floor);
 			linked_list_add_end(ll, new_node);
 		}
 
@@ -243,12 +261,15 @@ int main() {
 			Elevator* cur = elevators[i];
 			cur->total_wait_in_elev += cur->people_count;
 			printf("*** LOOKING AT ELEVATOR %d ON FLOOR %d WITH %d PEOPLE ON IT\n", cur->id, cur->floor, cur->people_count);
+			print_array(cur->riders, cur->people_count);
 
 			if (cur->stopped) {
 				printf("elevator %d is stopped\n", cur->id);
 				if (cur->time_waiting == 10) {
-					printf("elevator %d has waited for 10 seconds and/or is full, moving on\n", cur->id);
+					printf("elevator %d has waited for 10 seconds, moving on\n", cur->id);
 					cur->stopped = false;
+					elevator_here[cur->floor] = false;
+					cur->time_waiting = 0;
 					continue;
 				}
 				printf("elevator %d has waited for only %d seconds, picking up people on floor %d\n", cur->id, cur->time_waiting, cur->floor);
@@ -256,73 +277,95 @@ int main() {
 				cur->time_waiting++;
 			} else {
 				printf("elevator %d is moving\n", cur->id);
-				if (cur->people_count == 20) {
-					printf("Incrementing floor count\n");
-					cur->distance_traveled++;
-					cur->floor++;
-					continue;
-				}
-				if (floors[cur->floor]->size > 0 && cur->people_count == 0 && !elevator_here[cur->floor]) {
-					printf("stopping to pick up people on floor %d \n", cur->floor);
-					cur->stopped = true;
-					elevator_here[cur->floor] = true;
-				} else if (cur->upwards) { 
-					if (cur-> floor == 150) {
-						printf("Changing direction because floor is 150 or no riders\n");
-						cur->upwards = false;
-						continue;
-					}
-					printf("checking other conditions \n");
+				if (cur->upwards) { 
 					if (cur->riders[0] != NULL && cur->floor == cur->riders[0]->desired_floor) {
 						printf("stopping to drop off people on floor %d \n", cur->floor);
 						cur->stopped = true;
-					} else if (!keep_going(floors,cur->floor,cur->upwards) && cur->riders[0] == NULL) { // at end 
-						printf("Going down because no one is in elevator\n");
+						elevator_here[cur->floor] = true;
+						continue;
+					} 
+					if (cur->riders[19] !=NULL) {
+						printf("%d people_count and last person desired floor = %d\n", cur->people_count, cur->riders[19]->desired_floor);
+					}
+					if (cur->floor == 149 || (cur->people_count == 20 && cur->riders[19]->desired_floor == 0)) {
+						printf("UPWARDS: going down because at floor %d \n", cur->floor);
+						elevator_here[cur->floor] = true;
 						cur->upwards = false;
-					} else {
+						continue;
+					}
+					if (cur->people_count == 20) {
 						printf("Incrementing floor count\n");
 						cur->distance_traveled++;
 						cur->floor++;
+						continue;
 					}
+					if (floors[cur->floor]->size > 0 && !elevator_here[cur->floor]) {
+						printf("Stopping to pick up people\n");
+						elevator_here[cur->floor] = true;
+						cur->stopped = true;
+						continue;
+					}
+					printf("checking other conditions \n");
+					if (!keep_going(floors,cur->floor,cur->upwards) && cur->riders[0] == NULL) { // at end 
+						printf("Going down because no one is in elevator\n");
+						cur->upwards = false;
+						continue;
+					}
+					printf("Incrementing floor count\n");
+					printf("END OF THING: %d people_count\n", cur->people_count);
+					cur->distance_traveled++;
+					cur->floor++;
 				} else { // going down
+					if (cur->floor == 0) {
+						printf("DOWNWARDS: stopping to drop off people on floor %d \n", cur->floor);
+						elevator_here[cur->floor] = true;
+						cur->stopped = true;
+						cur->upwards = true;
+						continue;
+					}
 					if (cur->people_count == 20) {
-						printf("Incrementing floor count\n");
+						printf("Decrementing floor count\n");
 						cur->distance_traveled++;
 						cur->floor--;
 						continue;
 					}
-					if (cur->floor == 0) {
-						printf("DOWNWARDS: stopping to drop off people on floor %d \n", cur->floor);
+					if (floors[cur->floor]->size > 0 && !elevator_here[cur->floor]) {
+						elevator_here[cur->floor] = true;
 						cur->stopped = true;
-						cur->upwards = true;
+						continue;
 					}
-					if (cur->riders[0] != NULL && cur->floor == cur->riders[cur->people_count - 1]->desired_floor) {	
-						printf("stopping to drop off people on floor %d \n", cur->floor);
-						printf("\n");
-						cur->stopped = true;
-					} else if (!keep_going(floors, cur->floor, cur->upwards) || cur->riders[0] == NULL) { // at end or no more riders in elevator
-						printf("Changing direction because floor is 0 or no riders\n");
-						cur->upwards = true;
-					} else {
-						cur->distance_traveled++;
-						cur->floor--;
-					}
+					cur->distance_traveled++;
+					cur->floor--;
 				}
 			}
 		}
 
 		printf("incrementing wait time\n");
 		for (int i = 0; i < 150; i++) {
-			printf("i: %d\n", i);
+			// printf("floor %d\n", i);
 			linked_list* cur = floors[i];
+			// printf("%d\n", cur->size);
+			// printf("%p\n", cur);
 			increment_wait(cur);
 		}
 		time++;
 	}
 
+	long distance_traveled_tot = 0;
+	long total_wait_in = 0;
+	long total_wait_for = 0;
+	long tot_off = 0;
 	for (int i = 0; i < 8; i++) {
 		printf("Elevator %d distance traveled: %ld\n", i, elevators[i]->distance_traveled);
+		distance_traveled_tot += elevators[i]->distance_traveled;
 		printf("Elevator %d wait in elevator: %ld\n", i, elevators[i]->total_wait_in_elev);
-		printf("Elevator %d wait for elevator: %ld\n\n", i, elevators[i]->total_wait_for_elev);
+		total_wait_in += elevators[i]->total_wait_in_elev;
+		printf("Elevator %d wait for elevator: %ld\n", i, elevators[i]->total_wait_for_elev);
+		total_wait_for += elevators[i]->total_wait_for_elev;
+		printf("Elevator %d total off: %ld\n\n", i, elevators[i]->people_off);
+		tot_off = elevators[i]->people_off;
 	}
+	printf("Average distance traveled: %ld\n", distance_traveled_tot / tot_off);
+	printf("Average wait in elevator: %ld\n", total_wait_in / tot_off);
+	printf("Average wait for elevator: %ld\n", total_wait_for / tot_off);
 }
